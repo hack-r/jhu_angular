51b85c1b2b4039c8e2e4615644b968274e5f66e8
Jason Miller <root@hack-r.com>
2016-09-30 23:23:45 -0400

    works!

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,7 +1,66 @@
 (function () {
 'use strict';
 
-var shoppingList = [
+angular.module('ShoppingListApp', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.provider('ShoppingList', ShoppingListProvider)
+.config(Config);
+
+Config.$inject = ['ShoppingListProvider'];
+function Config(ShoppingListProvider) {
+  ShoppingListProvider.defaults.maxItems = 10;
+}
+
+ToBuyController.$inject = ['ShoppingList'];
+function ToBuyController(ShoppingList) {
+  var list = this;
+
+  list.items = ShoppingList.getItems();
+
+  list.itemName = "";
+  list.itemQuantity = "";
+
+  list.addItem = function () {
+    try {
+      ShoppingList.addItem(list.itemName, list.itemQuantity);
+    } catch (error) {
+      list.errorMessage = error.message;
+    }
+  }
+
+  list.removeItem = function (itemIndex) {
+    try{
+      ShoppingList.removeItem(itemIndex);
+    } catch(error){
+      list.errorMessage = error.message;
+    }
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingList'];
+function AlreadyBoughtController(ShoppingList){
+  var list = this;
+  list.bought = ShoppingList.getBought();
+
+  list.message = ""
+  console.log(list.bought.length);
+
+  list.itemName = "";
+  list.itemQuantity = "";
+
+  list.removeItem = function(itemIndex){
+    ShoppingList.removeItem(itemIndex);
+  };
+
+}
+
+// If not specified, maxItems assumed unlimited
+function ShoppingListService(maxItems) {
+  var service = this;
+
+  // List of shopping items
+  var items = [
   {
     name: "Cookies",
     quantity: 1
@@ -24,64 +83,57 @@ var shoppingList = [
   }
 ];
 
-angular.module('ShoppingListCheckOff', [])
-.controller('ToBuyController', ToBuyController)
-.controller('AlreadyBoughtController', AlreadyBoughtController)
-.service('ShoppingListCheckOffService', ShoppingListCheckOffService)
-//.service('ShoppingListService', ShoppingListService)
-;
+  var bought=[];
+  var empty=[];
 
-ToBuyController.$inject = ['$scope'];
-//ToBuyController.$inject = ['ShoppingListCheckOffService'];
-function ToBuyController($scope) {
-  $scope.shoppingList = shoppingList;
-}
+  service.addItem = function (itemName, quantity) {
+    if ((maxItems === undefined) ||
+        (maxItems !== undefined) && (items.length < maxItems)) {
+      var item = {
+        name: itemName,
+        quantity: quantity
+      };
+      items.push(item);
+    }
+    else {
+      throw new Error("Max items (" + maxItems + ") reached.");
+    }
+  };
 
-AlreadyBoughtController.$inject = ['$scope'];
-AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
-function AlreadyBoughtController($scope) {
-  $scope.shoppingList = shoppingList;
-  console.log(shoppingList);
-}
-function ShoppingListService() {
   service.removeItem = function (itemIndex) {
-  items.splice(itemIndex, 1);
+    var currItem = items[itemIndex];
+    bought.push(currItem);
+
+    items.splice(itemIndex, 1);
+    if(items.length > 0){void(0)
+    } else {
+          throw new Error("Nothing left to buy!");
+        }
+
   };
 
   service.getItems = function () {
     return items;
   };
-  return shoppingList;
+
+  service.getBought = function(){
+    return bought;
+  };
 }
-function ShoppingListCheckOffService() {
-  var shoppingList = [
-    {
-      name: "Cookies",
-      quantity: 1
-    },
-    {
-      name: "Milks",
-      quantity: 5
-    },
-    {
-      name: "vials of Crack Cocaine",
-      quantity: 2
-    },
-    {
-      name: "bottles of Vodka",
-      quantity: 3
-    },
-    {
-      name: "Eggs",
-      quantity: 1
-    }
-  ];
-  var factory = function () {
-    return new ShoppingListService();
-    //console.log();
+
+
+function ShoppingListProvider() {
+  var provider = this;
+
+  provider.defaults = {
+    maxItems: 100
   };
 
-  return factory;
+  provider.$get = function () {
+    var shoppingList = new ShoppingListService(provider.defaults.maxItems);
+
+    return shoppingList;
+  };
 }
 
 })();
